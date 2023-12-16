534de32b630dbb4a361c06939b1751cb599cd3df
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-16 05:03:23 +0200

    squash!

--- Source/basic-languages/dockerfile/dockerfile.contribution.ts
+++ Source/basic-languages/dockerfile/dockerfile.contribution.ts
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
 
 import { registerLanguage } from "../_.contribution";
 
@@ -9,19 +9,19 @@ declare var AMD: any;
 declare var require: any;
 
 registerLanguage({
-	id: "dockerfile",
-	extensions: [".dockerfile"],
-	filenames: ["Dockerfile"],
-	aliases: ["Dockerfile"],
-	loader: () => {
-		if (AMD) {
-			return new Promise((resolve, reject) => {
-				require([
-					"vs/basic-languages/dockerfile/dockerfile",
-				], resolve, reject);
-			});
-		} else {
-			return import("./dockerfile");
-		}
-	},
+id: "dockerfile",
+extensions: [".dockerfile"],
+filenames: ["Dockerfile"],
+aliases: ["Dockerfile"],
+loader: () => {
+if (AMD) {
+return new Promise((resolve, reject) => {
+require([
+"vs/basic-languages/dockerfile/dockerfile",
+], resolve, reject);
+});
+} else {
+return import("./dockerfile");
+}
+},
 });
